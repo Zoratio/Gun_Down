db476798850d561c68af80e8a57baaf69d045dbd
Taylor Bromley <taylorbromley@sky.com>
2021-10-27 17:18:25 +0100

    Dealing damage to actors

--- Source/Gun_Down/Gun.cpp
+++ Source/Gun_Down/Gun.cpp
@@ -40,8 +40,19 @@ void AGun::PullTrigger()
 	FHitResult Hit;
 	bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECC_GameTraceChannel1);
 
-	if (bSuccess) DrawDebugPoint(GetWorld(), Hit.Location, 20, FColor::Red, true);
-
+	if (bSuccess) 
+	{
+		FVector ShotDirection = -Rotation.Vector();
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactEffect, Hit.Location, ShotDirection.Rotation());
+		
+		AActor* HitActor = Hit.GetActor();
+		if (HitActor != nullptr)
+		{
+			FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
+			HitActor->TakeDamage(Damage,DamageEvent,OwnerController,this);
+		}
+		
+	}
 	//DrawDebugCamera(GetWorld(), Location, Rotation, 90, 2, FColor::Red, true);
 }
 
--- Source/Gun_Down/Gun.h
+++ Source/Gun_Down/Gun.h
@@ -36,5 +36,11 @@ private:
 	UParticleSystem* MuzzleFlash;
 
 	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactEffect;
+
+	UPROPERTY(EditAnywhere)
 	float MaxRange = 1000;
+
+	UPROPERTY(EditAnywhere)
+	float Damage = 10;
 };
